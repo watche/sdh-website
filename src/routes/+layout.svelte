e84ac2dc9ba81db06a165b9c6f4a6ab8d2423e04
Josh Randall <josh-randall@hush.ai>
2023-03-08 18:01:28 -0500

    Functioning (albeit poorly) hamburger menu.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
   import NavLink from '$routes/nav-link.svelte'
   import Footer from '$routes/footer.svelte'
   import DiscordButton from '$routes/discord-button.svelte'
+  import HamburgerMenu from '$components/hamburger-menu.svelte'
 
   const CONSTITUTION_ROUTE = "/constitution";
   const RULES_ROUTE = "/rules";
@@ -21,20 +22,34 @@
   }
   let logoActive;
   $: logoActive = ((route === "/") && !firstLoad);
+
+  let hamburgerMenuOpen = false;
 </script>
-<Navbar class="border-b dark:border-slate-200/5 mb-3">
-  <NavBrand href="/">
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white rounded-md p-2 logo-area" data-active={logoActive}>
-        <img src={SDHLogo} class="block" alt="SDH" />
-    </span>
-  </NavBrand>
-  <NavUl>
-    <NavLink route={CONSTITUTION_ROUTE}>Constitution</NavLink>
-    <NavLink route={RULES_ROUTE}>Rules</NavLink>
-    <NavLink route={DECKS_ROUTE}>Decks</NavLink>
-  </NavUl>
-  <DiscordButton />
-</Navbar>
+<span class="mb-3">
+  <Navbar class="border-b dark:border-slate-200/5">
+    <HamburgerMenu bind:open={hamburgerMenuOpen} />
+    <NavBrand href="/">
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white rounded-md p-2 logo-area" data-active={logoActive}>
+          <img src={SDHLogo} class="block" alt="SDH" />
+      </span>
+    </NavBrand>
+    <NavUl>
+      <NavLink route={CONSTITUTION_ROUTE}>Constitution</NavLink>
+      <NavLink route={RULES_ROUTE}>Rules</NavLink>
+      <NavLink route={DECKS_ROUTE}>Decks</NavLink>
+    </NavUl>
+    <DiscordButton />
+  </Navbar>
+  <div style="height: var(--hamburger-menu-height);" class={`${hamburgerMenuOpen?"hamburger-menu-expanded border-b":"hamburger-menu-collapsed"} dark:border-slate-200/5`}>
+    <Navbar>
+      <NavUl ulClass="flex-col">
+        <NavLink route={CONSTITUTION_ROUTE}>Constitution</NavLink>
+        <NavLink route={RULES_ROUTE}>Rules</NavLink>
+        <NavLink route={DECKS_ROUTE}>Decks</NavLink>
+      </NavUl>
+    </Navbar>
+  </div>
+</span>
 
 <!--
   This wrapper element, aside from creating columns to act as the margins,
@@ -59,5 +74,19 @@
   .logo-area:hover, .logo-area[data-active="true"] {
     /* mimicking bg-slate-700 */
     background-color: rgb(51 65 85);
+  }
+
+  .hamburger-menu-collapsed {
+    max-height: 0px;
+    overflow: hidden;
+    border-bottom-width: 1px;
+    border-bottom-width: 0px;
+    transition: max-height 0.25s linear, border-bottom-width 0.25s step-end;
+  }
+  .hamburger-menu-expanded {
+    max-height: 100px;
+    overflow: hidden;
+    border-bottom-width: 1px;
+    transition: max-height 0.25s linear, border-bottom-width 0.25s step-start;
   }
 </style>
