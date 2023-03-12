df57ee507617a7cbfe6d68c69c535f5a0d5710ad
Josh Randall <josh-randall@hush.ai>
2023-03-11 20:51:02 -0500

    Order deck DB alphabetically.

--- src/routes/decks/+page.svelte
+++ src/routes/decks/+page.svelte
@@ -21,8 +21,18 @@
       expandDeck(deck);
     }
   };
+
+  let sortedDecks = decks.slice(0).sort((deck1, deck2) => {
+    let name1 = deck1.name;
+    let name2 = deck2.name;
+    if(name1 > name2) return 1;
+    if(name1 < name2) return -1;
+    return 0;
+  });
 </script>
 <!--TODO: When reloading the page, the footer flashes in the wrong spot (at the bottom of the table.) -->
+<!--TODO: The avatar images flicker once when the page loads. -->
+<!--TODO? Create system for showing little badges on the right side of the deck row like "New" or a little medal for winning a game on Youtube or something. -->
 <svelte:head>
   <title>SDH Decklist Database</title>
   <meta name="description" content="A collection of interesting and/or representative decks of the SDH format in Magic: the Gathering." />
@@ -35,7 +45,7 @@
     1. The flowbite-svelte Table component appears to have no native support for rounded corners, and faking it with a wrapper div poses the challenge of making the wrapper div highlight with the top/bottom cells.
     2. Expanding a row underneath the selected row to display deck details is difficult to apply a CSS animation to in the context of a flowbite-svelte Table.
   -->
-  {#each decks as deck, i}
-    <DeckRow deck={deck} first={i===0} last={i===(decks.length-1)} on:click={() => (deckClick(deck))} expanded={deck.id === expandedDeck} />
+  {#each sortedDecks as deck, i}
+    <DeckRow deck={deck} first={i===0} last={i===(sortedDecks.length-1)} on:click={() => (deckClick(deck))} expanded={deck.id === expandedDeck} />
   {/each}
 </SDHWriting>
