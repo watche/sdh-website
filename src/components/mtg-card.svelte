20519dc527ecc7e9ec03385273667a9e679af14e
Josh Randall <josh-randall@hush.ai>
2023-03-12 19:51:55 -0400

    Add support for flip cards.

--- src/components/mtg-card.svelte
+++ src/components/mtg-card.svelte
@@ -2,7 +2,45 @@
   export let cardUrl;
   export let name;
   export let imageUrl;
+  export let flippable = false;
+  export let backSideImageUrl = "";
 
   import Link from '$components/link.svelte'
+
+  let flipped = false;
+  const flipCard = () => {
+    flipped = !flipped;
+  };
 </script>
-<Link href={cardUrl} class="cursor-pointer" nofollow><img style="border-radius: 20px" alt={name} src={imageUrl} /></Link>
+<Link href={cardUrl} class="cursor-pointer" nofollow>
+  <span id="card-container">
+    {#if flippable}
+      <button id="flip-button" on:click|stopPropagation|preventDefault={flipCard} >Flip</button>
+    {/if}
+    <img id="card-img" style="aspect-ratio: 334 / 465; border-radius: 20px;" height="465px" width="334px" alt={name} src={imageUrl} class={`${flipped?"flipped":""}`} />
+  </span>
+</Link>
+<style>
+  #card-container {
+    position: relative;
+  }
+  #flip-button {
+    position: absolute;
+    top: calc(50% - 1rem);
+    left: -1rem;
+    z-index: 3;
+    background-color: #00000099;
+    border-radius: 50%;
+    color: white;
+    border: 1px white solid;
+    padding: 1rem;
+  }
+  #card-img {
+    transform: rotate(0turn);
+    transition: transform 0.3s ease-out;
+  }
+  #card-img.flipped {
+    transform: rotate(0.5turn);
+    transition: transform 0.3s ease-out;
+  }
+</style>
