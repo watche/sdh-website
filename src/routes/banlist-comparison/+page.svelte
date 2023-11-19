24f7e258729df1a4dd8633459c7d1e27b665b979
Josh Randall <josh-randall@hush.ai>
2023-11-19 15:02:32 -0500

    Misc unbans

--- src/routes/banlist-comparison/+page.svelte
+++ src/routes/banlist-comparison/+page.svelte
@@ -1,3 +1,4 @@
+<!-- TODO: Make a JSON document containing the EDH banlist, and generate this based on a comparison (with commentary in its own document). -->
 <script context="module">
   import { writable } from 'svelte/store'
   const fromRules = writable(false);
@@ -25,14 +26,60 @@
         "name": "Biorhythm",
         "cardUrl": "https://scryfall.com/card/9ed/231/biorhythm",
         "imageUrl": "https://cards.scryfall.io/large/front/1/7/17d1a10f-ce21-4914-9984-c7c559161230.jpg?1593017425"
-      }
+      },
+      "banType": "everywhere",
+    },
+    {
+      "card": {
+        "name": "Emrakul, the Aeons Torn",
+        "imageUrl": "https://cards.scryfall.io/normal/front/2/4/249db4d4-2542-47ee-a216-e13ffbc2319c.jpg?1673146896",
+        "cardUrl": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=571334"
+      },
+      "banType": "everywhere",
+      "experimental": true,
+      "explanation": "Emrakul seems to be just exploitable enough to make the game more interesting without being centralizing. \n- Hardcasting it will very rarely happen. In most games, nobody gets to fifteen mana.\n- Because of the above, it will be a dead card in hand the majority of the time.\n- It's extremely difficult to cheat out with reanimation.\n- It can be used for infinite turn loops with Temur Sabertooth and the like.\n- In the Command Zone, it's mostly \"just worse\" than Thrasios."
+    },
+    {
+      "card": {
+        "name": "Leovold, Emissary of Trest",
+        "imageUrl": "https://cards.scryfall.io/normal/front/c/e/cedfc5b7-9242-4680-b284-debc8b5a9bc7.jpg?1559959275",
+        "cardUrl": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=456798"
+      },
+      "banType": "library",
+      "explanation": "While Leovold is a brutal stax piece, it's really not that out of place in the library next to Notion Thief and Opposition Agent."
+    },
+    {
+      "card": {
+        "name": "Recurring Nightmare",
+        "imageUrl": "https://cards.scryfall.io/normal/front/b/5/b50e1800-a45c-43bd-8886-8a06145d9346.jpg?1562431270",
+        "cardUrl": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=397441"
+      },
+      "banType": "everywhere",
+    },
+    {
+      "card": {
+        "name": "Gifts Ungiven",
+        "imageUrl": "https://cards.scryfall.io/normal/front/c/c/cc950b6c-0346-4939-b36b-9f10a75f7a32.jpg?1673147306",
+        "cardUrl": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=571384"
+      },
+      "banType": "everywhere",
+      "explanation": "Gifts Ungiven is very comparable to Intuition, and Intuition hasn't been a problem card in EDH."
+    },
+    {
+      "card": {
+        "name": "Rofellos, Llanowar Emissary",
+        "imageUrl": "https://cards.scryfall.io/normal/front/6/d/6d63a085-101c-4749-a3a4-1d335c25b10e.jpg?1562916904",
+        "cardUrl": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=383074"
+      },
+      "banType": "everywhere"
     },
     {
       "card": {
         "name": "Lutri, the Spellchaser",
         "cardUrl": "https://scryfall.com/card/iko/227/lutri-the-spellchaser",
         "imageUrl": "https://cards.scryfall.io/large/front/f/b/fb1189c9-7842-466e-8238-1e02677d8494.jpg?1628801771",
-      }
+      }, 
+      "banType": "everywhere"
     },
     {
       "card": {
@@ -40,6 +87,7 @@
         "cardUrl": "https://scryfall.com/card/mm2/20/iona-shield-of-emeria",
         "imageUrl": "https://cards.scryfall.io/large/front/6/1/6197b59e-1652-496c-a038-e2eb88ecf017.jpg?1562584407",
       },
+      "banType": "everywhere",
       "explanation": "Very hard to cast from the command zone, and not a desirable reanimator target compared to existing cards like Razaketh, Abdel, or Worldgorger Dragon. Its lock with Painter's Servant compares unfavorably to Lavinia's with Omen Machine."
     },
     {
@@ -48,44 +96,59 @@
         "cardUrl": "https://scryfall.com/card/vma/303/library-of-alexandria",
         "imageUrl": "https://cards.scryfall.io/large/front/e/5/e5145f31-a4ac-44ef-8f85-e4d95f2c9ff5.jpg?1562940986",
       },
-      "explanation": "Testing so far has shown Library of Alexandria to be a fun and interesting card, but by no means gamebreaking. It's maybe a little better than Urza's Saga."
+      "explanation": "Testing so far has shown Library of Alexandria to be a fun and interesting card, but by no means gamebreaking. It's maybe a little better than Urza's Saga.",
+      "banType": "everywhere",
     },
     {
       "card": {
         "name": "Primeval Titan",
         "cardUrl": "https://scryfall.com/card/ima/183/primeval-titan",
         "imageUrl": "https://cards.scryfall.io/large/front/6/d/6d5537da-112e-4679-a113-b5d7ce32a66b.jpg?1562850064",
-      }
+      },
+      "banType": "everywhere"
     },
     {
       "card": {
         "name": "Sway of the Stars",
         "cardUrl": "https://scryfall.com/card/bok/54/sway-of-the-stars",
         "imageUrl": "https://cards.scryfall.io/large/front/5/e/5eb58d9e-d181-4167-8b80-64b238183bdb.jpg?1562877344",
-      }
+      },
+      "banType": "everywhere"
     },
     {
       "card": {
         "name": "Sundering Titan",
         "cardUrl": "https://scryfall.com/card/2xm/292/sundering-titan",
         "imageUrl": "https://cards.scryfall.io/large/front/a/2/a2ebb5d3-72b1-411d-8c90-83dac5b37898.jpg?1618940470",
-      }
+      },
+      "banType": "everywhere"
     },
     {
       "card": {
         "name": "Sylvan Primordial",
         "cardUrl": "https://scryfall.com/card/gtc/136/sylvan-primordial",
         "imageUrl": "https://cards.scryfall.io/large/front/0/4/0483c869-38dc-4b0b-82f3-dd08a1ab985f.jpg?1561814269",
-      }
+      },
+      "banType": "everywhere"
     },
     {
       "card": {
         "name": "Upheaval",
         "cardUrl": "https://scryfall.com/card/mh2/270/upheaval",
         "imageUrl": "https://cards.scryfall.io/large/front/b/e/befe74b1-c487-42bb-a1a1-4d13f3a86ff7.jpg?1626100271",
-      }
+      },
+      "banType": "everywhere"
     }
   ];
+  const generalUnbans = unbans.filter((unban) => {
+    return unban.banType === "everywhere";
+  });
+  const commanderUnbans = unbans.filter((unban) => {
+    return unban.banType === "spouse";
+  });
+  const libraryUnbans = unbans.filter((unban) => {
+    return unban.banType === "library";
+  });
   const blankGoblin = {
     "name": "_____ Goblin",
     "imageUrl": "https://cards.scryfall.io/large/front/5/8/5819e3f3-da49-4003-88ce-f3b7bb495787.jpg?1673914269",
@@ -104,22 +167,40 @@
 </script>
 <svelte:head>
   <title>Bans: SDH vs. EDH</title>
-  <meta name="description" content="A list of cards banned/unbanned " />
+  <meta name="description" content="A list of cards banned/unbanned in SDH with different status in EDH." />
 </svelte:head>
 <SDHWriting>
   <span slot="title">Banlist Comparison to EDH</span>
   <aside class="dark:text-slate-700 test-sm">Note: The <Link href="/ban-list">Official SDH Banlist</Link> trumps this document if an inconsistency is ever created.</aside>
   <section>
-    <Subheading>Unbanned from Commander</Subheading>
-    <p>(These cards are unbanned both in the library and in the command zone.)</p>
+    <Subheading>New Unbans</Subheading>
+    <p>These cards are "generally" unbanned (unbanned in both the library the command zone).</p>
     <List class="inline">
-      {#each unbans as unban}
+      {#each generalUnbans as unban}
         <Li><CardWithExplanation card={unban.card} explanation={unban.explanation} /></Li>
       {/each}
     </List>
+    {#if commanderUnbans.length > 0}
+    <br />
+    <p>These cards are unbanned from the command zone specifically, and may still be banned in the library.</p>
+    <List class="inline">
+      {#each commanderUnbans as unban}
+        <Li><CardWithExplanation card={unban.card} explanation={unban.explanation} /></Li>
+      {/each}
+    </List>
+    {/if}
+    {#if libraryUnbans.length > 0}
+    <br />
+    <p>These cards are unbanned from use in the library, and may still be banned in the command zone.</p>
+    <List class="inline">
+      {#each libraryUnbans as unban}
+        <Li><CardWithExplanation card={unban.card} explanation={unban.explanation} /></Li>
+      {/each}
+    </List>
+    {/if}
   </section>
   <section>
-    <Subheading>Banned from SDH</Subheading>
+    <Subheading>New Bans</Subheading>
       <p>No cards have been banned yet.</p>
   </section>
   <section>
